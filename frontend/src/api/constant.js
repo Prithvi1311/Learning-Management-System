3f6fed86aaf4dec648e389fac3d5c11ed0f56f7d
Prithvi M <prithvim1311@gmail.com>
2025-12-19 21:56:53 +0530

    Committing fixes

--- frontend/src/api/constant.js
+++ frontend/src/api/constant.js
@@ -1,1 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+let apiUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+if (!apiUrl.startsWith("http")) {
+    apiUrl = `https://${apiUrl}`;
+}
+
+export const API_BASE_URL = apiUrl;
